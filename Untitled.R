0e36b5e97e3a83b24a2790cee9fb0379ef7dad38
BibhaDhungel <bievad@yahoo.com>
2020-03-23 16:16:35 +0900

    update

--- Untitled.R
+++ Untitled.R
@@ -1,3 +1,7 @@
+library(knitr)
+library(png)
+library(rmarkdown)
+install.packages("vembedr")
 
 rmarkdown::render_site()
 
@@ -7,35 +11,4 @@ install.packages('excelR')
 
 
 
-```{r echo = FALSE, results = 'asis', warning = FALSE}
-library(excelR)
-
-data = data.frame( Country = c('India', 'India', 'US','US'), 
-                   City = c('Bangalore', 'Mumbai', 'NY', 'SA'),
-                   A = c('Bangalore', 'Mumbai', 'NY', 'SA'),
-                   B = c('Bangalore', 'Mumbai', 'NY', 'SA'),
-                   C = c('Bangalore', 'Mumbai', 'NY', 'SA'),
-                   D = c('Bangalore', 'Mumbai', 'NY', 'SA'),
-                   E = c('Bangalore', 'Mumbai', 'NY', 'SA'))
-
-columns = data.frame(title = c('प्रान्त', 'कुल संख्या', 'अाज थप', 'मृत्यु', 'अाज मृ्त्यु',
-                               'निको', 'जटिल'),
-                     width = c(300, 300, 300, 300, 300, 300, 300),
-                     type = c('text', 'text', 'text', 'text', 'text', 'text', 
-                              'text'),
-                     source=I(list(0,c('Bangalore', 'Mumbai', 'NY', 
-                                       'SA','Delhi','Washington'))))
-excelTable(data=data, columns = columns)
-```
-
-url <- "https://ncov2019.live/data"
-local <- file.path("data", "demodata.data")
-download.file(url, local)
-my.data <- read.table(local, sep=",")
-str(my.data)
-
-
-library(readxl)
-data <- read_excel("data.xlsx")
-View(data)
 
